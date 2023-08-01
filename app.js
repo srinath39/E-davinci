2a42caf6a43dd2cd22c55e638eba12b6d15e0d98
msrinath <ashwinisrinath2580@gmail.com>
2023-08-01 22:29:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+const httpError = require("./models/http-error");
+
+const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images"))); // express.static is a middleware which is used to just return the requested file
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users/", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new httpError("could not find the route", 404);
+  return next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headersSent) {
+    return next(error);
+  } else {
+    res.status(error.code || 500);
+    res.json({ message: error.message || "sorry we could not find it" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ayke9fs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
